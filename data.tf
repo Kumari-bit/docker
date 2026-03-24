08d2b1612f551ba8f28f572f19c6b89961686e4b
Kumari <kumarivenkata994@gmail.com>
2026-03-24 21:59:09 +0530

    docker

---
+++ data.tf
@@ -0,0 +1,19 @@
+data "aws_ami" "joindevops" {
+  owners           = ["646418206500"]
+  most_recent      = true
+
+  filter {
+    name   = "name"
+    values = ["RHEL-9-DevOps-Practice"]
+  }
+
+  filter {
+    name   = "root-device-type"
+    values = ["ebs"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+}
